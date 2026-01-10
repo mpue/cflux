a4d06e3f1554ddeb462d250e9543cc5bca599525
mpue <matthias@pueski.de>
2026-01-10 12:11:37 +0100

    Logging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -53,7 +53,7 @@ app.use(cors({
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS : '+origin));
     }
   },
   credentials: true
